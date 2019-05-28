d439f3917da5a81189858459b33642c1ee30fbb6
Bartek Wisniewski <wis.keybar87@gmail.com>
2019-05-28 20:07:09 +0200

    Updates

---
+++ precache-manifest.645f329d63b6164af6baf83bf2815c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010d4d835e3bba6418b0e0bde1f37ebf",
+    "url": "/react-kanban/index.html"
+  },
+  {
+    "revision": "48383262b0a77e1a0698",
+    "url": "/react-kanban/static/css/main.38d0ebb9.chunk.css"
+  },
+  {
+    "revision": "7026516185f69eb61a47",
+    "url": "/react-kanban/static/js/2.7e2462ba.chunk.js"
+  },
+  {
+    "revision": "48383262b0a77e1a0698",
+    "url": "/react-kanban/static/js/main.0269d47c.chunk.js"
+  },
+  {
+    "revision": "2552bfe471c686e65fe7",
+    "url": "/react-kanban/static/js/runtime~main.57763009.js"
+  },
+  {
+    "revision": "a72acd2de62eae777ec2a14de1f8d00f",
+    "url": "/react-kanban/static/media/x.a72acd2d.svg"
+  }
+]);
